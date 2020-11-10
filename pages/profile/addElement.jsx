e252c685a4e81eb475dd4098f19ca10a36952d02
pavladan <fatalfenker@gmail.com>
2020-11-10 23:12:45 +0300

    add markdown preview on the adding page

--- pages/profile/addElement.jsx
+++ pages/profile/addElement.jsx
@@ -1,7 +1,8 @@
-import React,{useState} from "react";
-import { Form, Col, Button, Row } from "react-bootstrap";
-import axios from 'axios'
-import { useRouter } from 'next/router'
+import React, { useState } from "react";
+import { Form, Col, Button, Row, Tabs,Tab } from "react-bootstrap";
+import axios from "axios";
+import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
 
 const genre = [
   "Hurt",
@@ -24,45 +25,52 @@ const genre = [
 ];
 
 const AddElement = () => {
-	const [genres,setGenres] = useState(genre.map(e=>({name:e,value:false})));
+  const [genres, setGenres] = useState(
+    genre.map((e) => ({ name: e, value: false }))
+  );
   const [errorMsg, setErrorMsg] = useState();
-	const router = useRouter()
+  const [textForm, setTextForm] = useState("");
+  const router = useRouter();
 
   const handleSubmitFanFic = async (e) => {
     e.preventDefault();
-		const selectedGenres = genres.filter(e=>e.value===true).map(e=>e.name)
+    const selectedGenres = genres
+      .filter((e) => e.value === true)
+      .map((e) => e.name);
     const body = {
       name: e.currentTarget.nameFanfic.value,
-			description: e.currentTarget.description.value,
-			genres: selectedGenres,
-      text: e.currentTarget.text.value,
-		};
+      description: e.currentTarget.description.value,
+      genres: selectedGenres,
+      text: textForm,
+    };
     try {
-        const res = await axios.post("/api/user/posts", body);
-        if (res.status === 201) {
-						router.back();
-        }
+      const res = await axios.post("/api/user/posts", body);
+      if (res.status === 201) {
+        router.back();
+      }
     } catch (err) {
-        setErrorMsg(err.response.data);
+      setErrorMsg(err.response.data);
     }
   };
 
   return (
     <div className="forms-wrapper">
       <Form onSubmit={handleSubmitFanFic}>
-			{errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
+        {errorMsg ? <p style={{ color: "red" }}>{errorMsg}</p> : null}
         <Form.Group>
           <Form.Label>Name</Form.Label>
-          <Form.Control
-            name="nameFanfic"
-            placeholder="Name"
-            required
-          />
+          <Form.Control name="nameFanfic" placeholder="Name" required />
         </Form.Group>
 
         <Form.Group controlId="exampleForm.Description">
           <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" placeholder="Description" name="description" rows={3} required/>
+          <Form.Control
+            as="textarea"
+            placeholder="Description"
+            name="description"
+            rows={3}
+            required
+          />
         </Form.Group>
 
         <Form.Group as={Row}>
@@ -76,27 +84,39 @@ const AddElement = () => {
                 className="genre-element"
                 inline
                 type="checkbox"
-								label={val.name}
-								value={val.value}
-								onChange={()=>setGenres(old=>old.map(e=>{
-									if (e.name === val.name){
-										return {...e, value:!e.value}
-									}
-									return e
-								}))}
+                label={val.name}
+                value={val.value}
+                onChange={() =>
+                  setGenres((old) =>
+                    old.map((e) => {
+                      if (e.name === val.name) {
+                        return { ...e, value: !e.value };
+                      }
+                      return e;
+                    })
+                  )
+                }
               />
             ))}
           </Col>
         </Form.Group>
         <Form.Group controlId="exampleForm.Text">
           <Form.Label>Text</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            placeholder="Text"
-            name="text"
-            required
-          />
+          <Tabs defaultActiveKey="Write">
+            <Tab eventKey="Write" title="Write">
+              <Form.Control
+                as="textarea"
+								rows={5}
+                placeholder="Text"
+                value={textForm}
+                onChange={(e) => setTextForm(e.currentTarget.value)}
+                required
+              />
+            </Tab>
+            <Tab eventKey="Preview" title="Preview">
+              <ReactMarkdown>{textForm || "Nothing to preview"}</ReactMarkdown>
+            </Tab>
+          </Tabs>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
